0431e450700f68d65179deefd2be3bbf037f7ea4
Michael-Dyq <68020340+Michael-Dyq@users.noreply.github.com>
2021-04-12 13:36:50 -0400

    Trankit and cache are added

--- TAS/demo_stanza_texas/demo_prep.py
+++ TAS/demo_stanza_texas/demo_prep.py
@@ -1,7 +1,7 @@
-import stanza
-import spacy_udpipe
+# import stanza
+# import spacy_udpipe
 
-# STANZA: download English, Chinese, and Spanish model (takes a while)
+# # STANZA: download English, Chinese, and Spanish model (takes a while)
 # stanza.download('en')
 # stanza.download('zh')
 # stanza.download('es')
@@ -14,7 +14,7 @@ import spacy_udpipe
 # stanza.download("ar")
 # stanza.download('ru')
 
-# UDPIPE: download English, Chinese, and Spanish model (takes a while)
+# # UDPIPE: download English, Chinese, and Spanish model (takes a while)
 # spacy_udpipe.download("en")
 # spacy_udpipe.download("zh")
 # spacy_udpipe.download("es")
